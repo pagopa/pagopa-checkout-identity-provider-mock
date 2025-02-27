b884af2db6df62845d04735953e67e2ef2429c15
e.serrao <emanuele.serrao@bvtech.com>
2025-02-27 16:06:46 +0100

    converted rsa key private to v8

--- src/handlers/initMockHandler.ts
+++ src/handlers/initMockHandler.ts
@@ -2,7 +2,7 @@ import { RequestHandler } from "express";
 import { InitMock } from "../models/InitMock";
 import { logger } from "../logger";
 import { signJwtToken } from "../utils/rsa";
-import { getInMemoryState, setInMemoryState } from "../utils/inMemoryState";
+import { setInMemoryState } from "../utils/inMemoryState";
 
 const generateAuthCode = (length = 16) =>
     Array.from({ length }, () => 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.charAt(Math.floor(Math.random() * 52))).join('');
