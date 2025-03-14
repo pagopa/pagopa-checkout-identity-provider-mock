fa0ba4fbfb092807542aae4bd1bae96fcfbd8614
Simone Infante <52280205+infantesimone@users.noreply.github.com>
2025-03-14 07:10:39 +0100

    fix: delete async for health

--- src/handlers/healthHandler.ts
+++ src/handlers/healthHandler.ts
@@ -1,11 +1,12 @@
 import { RequestHandler } from "express";
 
-export const liveness: RequestHandler = async (_, res) => {
+export const liveness: RequestHandler = (_, res) => {
     res.setHeader("Content-Type", "application/json");
     res.status(200).send({"Liveness": "OK"});
 };
   
-export const readiness: RequestHandler = async (_, res) => {
+export const readiness: RequestHandler = (_, res) => {
     res.setHeader("Content-Type", "application/json");
     res.status(200).send({"Readiness": "OK"});
 };
+
